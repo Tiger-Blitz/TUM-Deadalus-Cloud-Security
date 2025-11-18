aaf2782bac3eded4703536a65c18a676f4401ffd
Tiger-Blitz <anthtz@orange.fr>
2025-11-18 17:13:08 +0100

    Add second image via Terraform

--- s3.tf
+++ s3.tf
@@ -23,3 +23,9 @@ resource "aws_s3_object" "fruit_salad_image" {
   key    = "fruitsalad.png"
   source = "image/fruitsalad.png"
 }
+
+resource "aws_s3_object" "emoji_s3_object" {
+  bucket = aws_s3_bucket.web_bucket.id
+  key    = "emoji.png"
+  source = "image/emoji.png"
+}
